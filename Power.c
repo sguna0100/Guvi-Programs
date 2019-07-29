a9a49d176a659158c969b258fe107a67caa70972
Gunasekaran S <37099075+sguna0100@users.noreply.github.com>
2019-07-29 22:22:46 +0530

    Update Power.c

--- Power.c
+++ Power.c
@@ -1,14 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+
 int main()
-	{
-		int n,m;
-		long res=1;
-		scanf("%d %d",&n,&m);
-		while(m>0)
-		{
-			res*=n;
-			--m;
-		}
-		printf("%ld",res);
-	}
+{
+    int base, exponent;
+    long long power = 1;
+    int i;
+    scanf("%d", &base);
+    scanf("%d", &exponent);
+    for(i=1; i<=exponent; i++)
+    {
+        power = power * base;
+    }
+    printf("%lld",power);
+    return 0;
+}
