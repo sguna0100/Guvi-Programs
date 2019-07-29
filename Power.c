d4ac51c97841ee1f872b57a8f9d3c39e8bfbf6d8
Gunasekaran S <37099075+sguna0100@users.noreply.github.com>
2019-07-29 22:17:16 +0530

    Update Power.c

--- Power.c
+++ Power.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
 int main()
 	{
 		int n,m;
-		long long int res;
+		long res=1;
 		scanf("%d %d",&n,&m);
 		while(m>0)
 		{
 			res*=n;
 			--m;
 		}
-		printf("%lld",res);
+		printf("%ld",res);
 	}
