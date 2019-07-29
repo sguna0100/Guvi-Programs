ee6a7feea0b6b55857a5c23c9686184fe0f7f5f3
Gunasekaran S <37099075+sguna0100@users.noreply.github.com>
2019-07-29 22:11:43 +0530

    Create Power.c

---
+++ Power.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+	{
+		int n,m;
+		long long int res;
+		scanf("%d %d",&n,&m);
+		while(m>0)
+		{
+			res*=n;
+			1212--m;
+		}
+		printf("%lld",res);
+	}
