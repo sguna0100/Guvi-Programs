0c4201c5a863056fa842eb986ab74872167963b0
Gunasekaran S <37099075+sguna0100@users.noreply.github.com>
2019-07-31 22:20:01 +0530

    Create Arr_Min.c

---
+++ Arr_Min.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+int main()
+{
+    int arr[MAX_SIZE];
+    int i,min,size;
+    scanf("%d",&size);
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    min=arr[0];
+    for(i=1;i<size;i++)
+    {
+        if(arr[i]<min)
+        {
+            min=arr[i];
+        }
+    }
+    printf("%d",min);
+    return 0;
+}
