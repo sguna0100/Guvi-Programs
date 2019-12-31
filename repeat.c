f91d937cb56e176f0ee7ef61e410dac0078cdf1d
Gunasekaran S <37099075+sguna0100@users.noreply.github.com>
2019-12-31 14:23:38 +0530

    Create repeat.c

---
+++ repeat.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+main()
+{
+ int num,k,temp,frequency[9],flag=0,i;
+ scanf("%d",&num);
+ temp=num;
+ for(i=0;i<10;i++)
+ {
+    frequency[i]=0;
+ }
+ while(num>0)
+ {
+  k=num%10;
+  frequency[k]++;
+  num/=10;  
+ }
+ for(i=0;i<10;i++)
+ {
+  if(frequency[i]>1)
+  {
+   flag=1;
+  }
+ }
+ if(flag==0)
+ {
+  printf("no");
+ }
+ else
+ {
+  printf("yes");
+ }
+}
